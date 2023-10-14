60027ebc433c390be019cc820ff4447fafdc0dbd
Axolotlguy1321 <147824338+Axolotlguy1321@users.noreply.github.com>
2023-10-14 06:46:58 -0500

    Add files via upload
    These are all of the scripts for the game!!!

---
+++ scriptsInEvents.js
@@ -0,0 +1,25 @@
+
+
+
+const scriptsInEvents = {
+
+	async Maincode_Event21_Act1(runtime, localVars)
+	{
+		// Makes the red spikes annoy you
+		alert ("Red Spikes dont hurt you, but they annoy you with this message, your Welcome!!!1")
+	},
+
+	async TheEnd_Event1_Act1(runtime, localVars)
+	{
+		window.open("https://axolotlguy1321.itch.io/cubey-the-cube")
+	},
+
+	async TheEnd_Event2_Act1(runtime, localVars)
+	{
+		window.open("https://axolotlguy1321.itch.io/cubey-the-cube")
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
